3ff3b76da21f2a6e8277dccb59a4d97d749c97b4
TRZ-262 <84151405+TRZ-262@users.noreply.github.com>
2024-10-18 20:07:47 -0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\LDSW\\welcome_to_flutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "PROJECT_DIR=C:\\LDSW\\welcome_to_flutter"
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\LDSW\\welcome_to_flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\LDSW\\welcome_to_flutter"
+  "FLUTTER_TARGET=C:\\LDSW\\welcome_to_flutter\\lib\\LDSW-3.4_UTILIZACION_WIDGETS.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\LDSW\\welcome_to_flutter\\.dart_tool\\package_config.json"
+)
